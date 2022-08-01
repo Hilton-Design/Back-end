ab5c47ad9074efc85f92b2c7c7d31b921fce9953
leekyukin <leekujin14@gmail.com>
2022-08-01 19:39:14 +0900

    SET :: Initial

---
+++ src/main/java/com/hilton/hibye/HiByeServerApplication.java
@@ -0,0 +1,13 @@
+package com.hilton.hibye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiByeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HiByeServerApplication.class, args);
+    }
+
+}
