1e13ef77da12355b60134025c12bd8df94eccae7
leekyukin <leekujin14@gmail.com>
2022-08-01 21:22:26 +0900

    ADD :: User Entity extends BaseTimeEntity

--- src/main/java/com/hilton/hibye/domain/user/domain/User.java
+++ src/main/java/com/hilton/hibye/domain/user/domain/User.java
@@ -1,11 +1,48 @@
 package com.hilton.hibye.domain.user.domain;
 
+import com.hilton.hibye.domain.user.domain.type.Role;
+import com.hilton.hibye.global.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 @Entity
 @Table
-public class User {
+@NoArgsConstructor
+public class User extends BaseTimeEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private int age;
+
+    private String phone;
+
+    private String address;
+
+    private int lateCount;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Builder
+    public User (String name, String email, String password, int age, String phone, String address) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.age = age;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    public void late() {
+        this.lateCount += 1;
+    }
 }
---
+++ src/main/java/com/hilton/hibye/domain/user/domain/type/Role.java
@@ -0,0 +1,8 @@
+package com.hilton.hibye.domain.user.domain.type;
+
+import lombok.Getter;
+
+@Getter
+public enum Role {
+    USER, ADMIN
+}
