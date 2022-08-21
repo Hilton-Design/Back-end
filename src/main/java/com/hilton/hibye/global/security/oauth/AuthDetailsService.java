5292044bb6a7fbbfdd3ecda1525d04272bf795f5
leekyukin <leekujin14@gmail.com>
2022-08-21 23:43:09 +0900

    ADD :: Auth

---
+++ src/main/java/com/hilton/hibye/global/security/oauth/AuthDetailsService.java
@@ -0,0 +1,23 @@
+package com.hilton.hibye.global.security.oauth;
+
+import com.hilton.hibye.domain.user.domain.repository.UserRepository;
+import com.hilton.hibye.domain.user.exception.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .map(AuthDetails::new)
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+    }
+}
